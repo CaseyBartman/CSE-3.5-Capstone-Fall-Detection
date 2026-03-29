fdd7667bc5eeddcc283f5659fbc84658a5cc2ebe
Casey Bartman <bartman.26@osu.edu>
2026-03-29 19:46:07 -0400

    Improving name of test endpoint to match actual one

--- test/TestConstants.cpp
+++ test/TestConstants.cpp
@@ -1,7 +1,7 @@
 #include "test/TestConstants.h"
 
 // NTFY Testing Values. Initialized here to prevent linker issues from conflicting with production code
-const char* TEST_NTFY_ENDPOINT = "http://ntfy.sh/test_topic";
+const char* TEST_NTFY_ENDPOINT = "http://ntfy.sh/test_patient_fall_alert_5911";
 const char* EXPECTED_FALL_PAYLOAD = "PATIENT HAS FALLEN!";
 const char* EXPECTED_CLEAR_PAYLOAD = "ALARM CLEARED";
 const char* EXPECTED_CONTENT_TYPE = "text/plain";
